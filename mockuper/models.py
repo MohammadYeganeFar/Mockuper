435f93ea96df1d4bcff5e9328fe7759d2fef36a7
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-01 20:27:56 +0330

    Add Model MockupImage

--- mockuper/models.py
+++ mockuper/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class MockupImage(models.Model):
+    text = models.TextField()
+    image = models.ImageField(upload_to='mockup_images')
+    created_at = models.DateTimeField(auto_now_add=True)
