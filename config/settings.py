1fb83bcdb0f6f65dc9555043b44045cd32f48cff
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-01 19:49:24 +0330

    Initiate Celery configs

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/1'
+
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/1'
