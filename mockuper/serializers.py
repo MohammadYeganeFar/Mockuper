b7c14bc7fe06fc6207e245c83133e23adeeaf5a8
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-02 20:21:39 +0330

    Update image URL generation in MockupTaskSerializer to use dynamic base URL from request context

--- mockuper/serializers.py
+++ mockuper/serializers.py
@@ -18,13 +18,17 @@ class MockupTaskSerializer(serializers.ModelSerializer):
 
     def get_results(self, obj):
         images = obj.images.all()
+        request = self.context.get('request')
+        base_url = request.build_absolute_uri('/')[:-1] if request else 'http://127.0.0.1:8000'
+        
         data = [
             {
-                "image_url": 'http://127.0.0.1:8000/' + image.url,
+                "image_url": f"{base_url}/{image.url}" if not image.url.startswith('http') else image.url,
                 "created_at": image.created_at,
             } for image in images
         ]
         return data
+
 
 class MockupImageHistorySerializer(serializers.ModelSerializer):
     class Meta:
