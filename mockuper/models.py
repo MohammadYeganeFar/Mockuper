017ed459e8e078e4757db88ea180b9fe5ff0326e
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-02 10:55:20 +0330

    Update MockupImage model

--- mockuper/models.py
+++ mockuper/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class MockupImage(models.Model):
     text = models.TextField()
-    image = models.ImageField(upload_to='mockup_images')
+    url = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
