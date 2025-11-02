47712be2cd9dd4d587a59c365789f21a090ec131
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-02 18:39:37 +0330

    Update settings.py: Replace hardcoded SECRET_KEY with environment variable

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@6i%7aosxjtv7a7netr^w^!1d&qy(vqb&17k4s1p$hfb=8_kg3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
