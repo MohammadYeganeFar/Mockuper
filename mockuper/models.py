e1affc7c9ee4e910d10dc58572d4dfbb54488a7f
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-02 12:28:58 +0330

    Add model MockupTask

--- mockuper/models.py
+++ mockuper/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class MockupTask(models.Model):
+    STATUS_CHOICES = {
+        'PENDING': 'Pending',
+        'STARTED': 'Started',
+        'RETRY': 'Retry',
+        'FAILURE': 'Failure',
+        'SUCCESS': 'Success' 
+    }
+    task_id = models.UUIDField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
 
 class MockupImage(models.Model):
     text = models.TextField()
