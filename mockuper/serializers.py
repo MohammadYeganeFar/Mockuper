fadb9fc553afceb7515dffacdb064cc4f7df06bc
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-02 17:04:58 +0330

    Add MockupTaskSerializer for using in get_task_status view

--- mockuper/serializers.py
+++ mockuper/serializers.py
@@ -9,3 +9,19 @@ class MockupImageSerializer(serializers.ModelSerializer):
         fields = ['text']
 
 
+class MockupTaskSerializer(serializers.ModelSerializer):
+    results = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.MockupTask
+        fields = ['task_uuid', 'status', 'results']
+
+    def get_results(self, obj):
+        images = obj.images.all()
+        data = [
+            {
+                "image_url": image.url,
+                "created_at": image.created_at,
+            } for image in images
+        ]
+        return data
