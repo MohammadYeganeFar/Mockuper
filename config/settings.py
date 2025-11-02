3a435d5e85ab70df59effe77f1f98873a48e2270
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-02 19:01:48 +0330

    Add MEDIA_URL and MEDIA_ROOT to settings and use them in config/urls.py for serving media files

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379/1'
 
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/1'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
