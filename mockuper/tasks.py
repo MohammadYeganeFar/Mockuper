e623ed55ca00265b30a47a29262e8126f4de39cf
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-02 11:00:44 +0330

    Update create_mockup tasks to use .env variables

--- mockuper/tasks.py
+++ mockuper/tasks.py
@@ -1,10 +1,17 @@
-from celery import shared_task 
+import os
+from dotenv import load_dotenv
+from random import randint
+from celery import shared_task , group
 from PIL import Image, ImageDraw
+
+load_dotenv()
 
 
 @shared_task
-def create_mockup(text, img_path):
-    img = Image(img_path)
+def create_mockup(text, input_path):
+    img = Image.open(input_path)
     painter = ImageDraw.Draw(img)
     painter.text((300, 300), text, fill=(255, 0, 0))
-    painter.save('/home/mohammad-yegane-far/programming/Mockuper/created_images')
+    path_to_save = os.environ.get('GENERATED_IMAGES') + str(randint(1, 1000)) + '.png'
+    img.save(path_to_save)
+    return 1
