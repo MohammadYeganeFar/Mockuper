624cb5fb2ef72cc34069fd35d463ba5e8994c717
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-11-02 15:38:27 +0330

    Update generate_mockup_shirt: create task inside view

--- mockuper/views.py
+++ mockuper/views.py
@@ -3,24 +3,40 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from celery import group
+from celery.result import AsyncResult
 from mockuper import serializers
 from mockuper import tasks
+from mockuper import models
 
 
 @api_view(['POST'])
 def generate_mockup_shirt(request):
+    # Serializing the data
     serializer = serializers.MockupImageSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     text = request.data['text']
+
+    # Creating the tasks
     files = os.listdir('mockuper/shirt_images')
     images_abs_path = [os.environ.get('SAMPLE_IMAGES') + file for file in files]
-    tasks_signatures = [tasks.create_mockup.s(text, path) for path in images_abs_path]
+
+    # Saving the task to the database
+    task = models.MockupTask.objects.create()
+
+    # Creating the tasks signatures
+    tasks_signatures = [tasks.create_mockup.s(text, path, task.id) for path in images_abs_path]
+    # Creating the multiple mockup tasks
     multiple_mockup_tasks = group(tasks_signatures)
+    # Executing the tasks
     executed_tasks = multiple_mockup_tasks.delay()
-    # mockup_image = serializer.save(url=task)
+    
+    task.task_id = executed_tasks.id
+    res = AsyncResult(executed_tasks.id)
+    task.status = res.state
+    task.save()
     data = {
         'task_id': executed_tasks.id,
-        'status': 'PENDING',
+        'status': res.state,
         'message': 'ساخت تصویر آغاز شد'
     }
     return Response(data, status.HTTP_201_CREATED)
